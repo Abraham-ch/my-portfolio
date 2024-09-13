a9f9aed34a31b525be3cb3712bf1984de5322e9b
Abraham-ch <abraham.chafloque.a@uni.pe>
2024-09-13 14:46:59 -0500

    feat: adding description and links in Projects

--- src/sections/InTouch.jsx
+++ src/sections/InTouch.jsx
@@ -2,10 +2,10 @@ const InTouch = () => {
   return (
     <section
         id="contact"
-        className="md:flex md:justify-between pb-2 bg-slate-200 dark:bg-slate-800 mt-10 mb-20 flex-cols"
+        className="md:flex h-full md:justify-between pb-2 bg-slate-200 dark:bg-slate-800 mt-10 mb-20 flex-cols"
       >
-        <div className="px-10 py-10">
-          <h2 className="text-2xl font-bold text-blue-950 px-7 pt-10 dark:text-yellow-400">
+        <div className="px-10 h-full my-auto">
+          <h2 className="text-2xl font-bold text-blue-950 px-7 dark:text-yellow-400">
             Get in touch
           </h2>
           <p className="px-7 pt-8 max-w-lg dark:text-gray-200">
